2a81b0580a37a867e77ca51d585b05359107eb21
Anakin <44977098+AnD13@users.noreply.github.com>
2022-02-17 21:00:00 +0300

    Add Project

---
+++ replace.js
@@ -0,0 +1,14 @@
+// https://learn.javascript.ru/types
+"use strict"; // Будем использовать современный код. Если убрать директиву, то будет работать только для старого
+
+const MY_NAME = "Семён";
+const MY_AGE = 18;
+let message = "Hello мир";
+
+// let message = '6566 мир';
+// alert("Привет мир"); появляется окно с надписью Привет мир
+// alert(`Меня зовут ${MY_NAME}, мне ${MY_AGE}`); 
+// let isGreater = 4 > 1;
+// typeof("foo") // "string"  // аналог в питоне type("fool")
+alert(`Меня зовут ${MY_NAME}, мне ${MY_AGE}`);
+// [1, 2].forEach(alert);
