548155f8caf0f6ff7392722f6e9f1ccb594caecf
Anakin <None@gmail.com>
2024-03-28 22:41:54 +0300

    chore: useful examples added

--- Main.js
+++ Main.js
@@ -15,3 +15,27 @@
         iconElement.setAttribute('aria-label', 'Самое популярное ссылка');
     }
 })();
+
+// для скрытия озвучки
+// aria-hidden="true"
+
+// Для автоматического автофокуса на элементе при перезагрузке страницы
+// <h1 autofocus>Заголовок страницы</h1>
+
+// Изменяем последовательность озвучки
+// <div tabindex="2">Элемент 2</div>
+// <div tabindex="1">Элемент 1</div>
+// <div tabindex="3">Элемент 3</div>
+// <div tabindex="4">Элемент 4</div>
+// <div tabindex="6">Элемент 6</div>
+// <div tabindex="7">Элемент 7</div>
+// <div tabindex="8">Элемент 8</div>
+// <div tabindex="5">Элемент 5</div>
+// <div tabindex="9">Элемент 9</div>
+// <div tabindex="10">Элемент 10</div>
+
+// https://youtu.be/U4dSWJFIQ0A?t=1744
+// ожидание загрузки страницы
+// window.addEventListener('load', function() {
+//     //code
+// });
